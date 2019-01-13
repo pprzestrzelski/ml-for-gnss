856c9bf265f0d18975a248fb448f4891ca14b6a9
Pawel Przestrzelski <pawel.przestrzelski@gmail.com>
2019-01-13 16:44:56 +0100

    Add example on how to use LinearEstimator

---
+++ example2.py
@@ -0,0 +1,41 @@
+# DESCRIPTION
+# Simple demonstration of various estimators available in LinearEstimator.py
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from gnss_clock_data import GnssClockData
+from LinearEstimator import LinearEstimator
+
+SCALE = 10.0 ** 9
+
+if __name__ == '__main__':
+    clock_data = GnssClockData()
+    sat_number = 'G09'
+    data = clock_data.get_satellite_data(sat_number)
+    epochs = []
+    clock_biases = []
+    for epoch, clock_bias in data:
+        epochs.append(epoch)
+        clock_biases.append(float(clock_bias.get_bias()) * SCALE)
+
+    X = np.array(epochs).reshape(-1, 1)
+    y = np.array(clock_biases)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, shuffle=False)
+
+    estimators = \
+        {'Linear': LinearEstimator(X_train, X_test, y_train, y_test, sat_number),
+         'Poly-2nd': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='OLS', degree=2),
+         'Poly-4th': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='OLS', degree=4),
+         'Poly-8th': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='OLS', degree=8),
+         'Theil-Sen': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='Theil-Sen'),
+         'RANSAC': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='RANSAC'),
+         'HuberRegressor': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='HuberRegressor'),
+         'SGD': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='SGD'),
+         'Ridge': LinearEstimator(X_train, X_test, y_train, y_test, sat_number, estimator='Ridge')}
+
+    for name, est in estimators.items():
+        est.fit()
+        est.predict()
+        res = est.stats()
+        mae, mse, rms = res
+        print("%15s => mae: %.4f, mse: %.4f, rms: %.4f" % (name, mae, mse, rms))
