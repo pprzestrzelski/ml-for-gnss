a41643a4bcb7aa9b79c2bd53655c87dcceef4b98
Pawel Przestrzelski <pawel.przestrzelski@gmail.com>
2019-02-21 22:56:23 +0100

    Create function converting UTC time to GPS week and GPS day

---
+++ scripts/core/utils/gnss_time.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def utc_to_gps_week_day(utc_time, utc_format):
+    gps_zero_epoch = datetime.strptime("1980-01-07 00:00:00", "%Y-%m-%d %H:%M:%S")
+    utc = datetime.strptime(utc_time, utc_format)
+
+    diff = utc - gps_zero_epoch
+    gps_week = diff / 7
+    gps_day = utc.isoweekday() if utc.isoweekday() != 7 else 0
+
+    return gps_week, gps_day
