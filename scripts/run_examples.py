512fc8f6eb761858532c79616c306efffd9983a9
Pawel Przestrzelski <pawel.przestrzelski@gmail.com>
2019-02-24 21:03:29 +0100

    Add new example with Sp3 files and get rid of const upper limit of selectable options

--- scripts/run_examples.py
+++ scripts/run_examples.py
@@ -1,13 +1,14 @@
 import examples.example1 as e1
 import examples.example2 as e2
 import examples.example3 as e3
+import examples.example4 as e4
 import examples.scratchpad as sc
 
-ex = {1: e1.main, 2: e2.main, 3: e3.main, 4: sc.main}
+ex = {1: e1.main, 2: e2.main, 3: e3.main, 4: e4.main, 5: sc.main}
 
 
 def main():
-    print('Select example (1-4):')
+    print('Select example (1-{}):'.format(len(ex)))
     try:
         prompt = input('>')
         selection = int(prompt)
@@ -15,7 +16,7 @@ def main():
     except ValueError:
         print('Selection must be a number')
     except KeyError:
-        print('Choose value from range 1 to 3')
+        print('Choose value from range 1 to {}'.format(len(ex)))
 
 
 if __name__ == '__main__':
