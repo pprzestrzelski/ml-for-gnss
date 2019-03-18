b5bbe922de69946315edf01e58b4335778a9b438
Regule <regule@zoho.com>
2019-03-18 19:05:25 +0100

    Cleaned up code, added requirements

--- scripts/reccurent.py
+++ scripts/reccurent.py
@@ -3,6 +3,23 @@ import numpy as np
 import tensorflow as tf
 import sys
 
+class Config:
+
+    def __init__(self, sequence_size, batch_size, hidden_size, sequence_shift,
+                 loss_function, steps_per_epoch, epochs):
+        self.sequence_size = sequence_size
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+        self.sequence_shift = sequence_shift
+        self.loss_function = loss_function
+        self.steps_per_epoch = steps_per_epoch
+        self.epochs = epochs
+
+
+    @staticmethod
+    def demo_config():
+        return Config(10, 3, 10, 1, 'mean_squared_error', 100, 10)
+
 
 class BatchGenerator:
 
@@ -38,18 +55,37 @@ class BatchGenerator:
         data = pd.read_csv(filename)
         return BatchGenerator(data.values)
 
+
+class NeuralNetwork:
+
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, generator, cfg):
+        # FIXME: Make checkpoints work
+        #checkpointer = tf.keras.callbacks.ModelCheckpoint(filepath='checkopints' + '/model-{epoch:02d}.hdf5', verbose=1)
+        self.model.fit_generator(generator.generate(),
+                                 steps_per_epoch=cfg.steps_per_epoch,
+                                 epochs=cfg.epochs)
+
+    @staticmethod
+    def build_lstm_model(cfg):
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.LSTM(cfg.sequence_size,
+                                       return_sequences=True,
+                                       input_shape=(cfg.batch_size, cfg.sequence_size)))
+        model.add(tf.keras.layers.LSTM(cfg.hidden_size, return_sequences=True))
+        model.compile(loss=cfg.loss_function, optimizer='adam', metrics=['mae'])
+        return NeuralNetwork(model)
+
 def main():
     input_csv = None
-    sequence_size = 10
-    batch_size = 3
-    hidden_size = 10
-    gen = BatchGenerator.random_data(1000,sequence_size,batch_size)
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.LSTM(sequence_size, return_sequences=True, input_shape=(batch_size,sequence_size)))
-    model.add(tf.keras.layers.LSTM(hidden_size, return_sequences=True))
-    #model.add(tf.keras.layers.Activation('LeakyReLU'))
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
-    model.fit_generator(gen.generate(), steps_per_epoch=100, epochs=10)
+    cfg = Config.demo_config()
+    gen = BatchGenerator.random_data(1000, cfg.sequence_size, cfg.batch_size)
+    nn = NeuralNetwork.build_lstm_model(cfg)
+    nn.fit(gen, cfg)
+
+
 
 if __name__ == '__main__':
     main()
