cea5c022110d6d1742dafe927ba6ed639abfdc43
regule <regule@zoho.com>
2019-05-12 08:24:27 +0200

    Better data loading

--- scripts/ugly.py
+++ scripts/ugly.py
@@ -7,6 +7,9 @@ from math import floor
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import GridSearchCV
+import keras
+from keras.initializers import glorot_uniform
+from keras.utils import CustomObjectScope
 
 
 # Inicjalizujemy zmienne globalne do późniejszego użytku
@@ -66,6 +69,19 @@ def prepare_data():
 
     return data 
 
+
+# Funkcja genereuje pary wejście-wyjście dla sieci neuronowej na podstawie
+# ciągu. Wyjscie jest zawsze przesunięte względem wejścia o jeden element
+def time_series_to_input_and_output(ts, input_len):
+    inputs_count = len(ts)-net_input_len
+    X=[]
+    Y=[]
+    for i in range(inputs_count):
+        X.append(data[i:i+net_input_len])
+        Y.append(data[i+1:i+net_input_len+1])
+    return np.asarray(X), np.asarray(Y)
+
+
 # Funkcja tworząca sieci neuronowe, wymagana do współpracy
 # z pakietem scikit learn
 def create_network(optimizer='adam', batch_size=10):
@@ -86,19 +102,11 @@ def create_network(optimizer='adam', batch_size=10):
 # Szuka najlepszych parametrów dla sieci neuronowej
 def find_best_parameters():
     global net_input_len
+
     # Żeby zastosować dostrajanie sieci z scikit musimy wygenerować pary
     # wejście-wyjście dla ciągu
-    X = []
-    Y = []
-    net_input_len = int(input('Długość wejścia sieci >'))
-    inputs_count = (len(data)-1) // net_input_len
-    for i in range(inputs_count):
-        shift = i*net_input_len
-        X.append(data[shift:shift+net_input_len])
-        Y.append(data[shift+1:shift+net_input_len+1])
-    X = np.asarray(X)
-    Y = np.asarray(Y)
-
+    X, Y = time_series_to_input_and_output(data, net_input_len)
+    
     # Reshape wymagany dla warstwy LSTM w kerasie
     X = X.reshape(X.shape[0], 1, X.shape[1])
 
@@ -122,18 +130,8 @@ def find_best_parameters():
 
 def teach_and_evaluate():
     global net_input_len
-    # Żeby zastosować dostrajanie sieci z scikit musimy wygenerować pary
-    # wejście-wyjście dla ciągu
-    X = []
-    Y = []
-    net_input_len = int(input('Długość wejścia sieci >'))
-    inputs_count = (len(data)-1) // net_input_len
-    for i in range(inputs_count):
-        shift = i*net_input_len
-        X.append(data[shift:shift+net_input_len])
-        Y.append(data[shift+1:shift+net_input_len+1])
-    X = np.asarray(X)
-    Y = np.asarray(Y)
+
+    X, Y = time_series_to_input_and_output(data, net_input_len)
 
     # Dzielimy teraz te pary na część treningową i testową
     tr_count = float(input('Jaka część danych zostanie użyta do uczenia (0;1) >'))
@@ -160,15 +158,39 @@ def teach_and_evaluate():
     nn.fit(X_tr, Y_tr, epochs=epochs, batch_size=batch_size, validation_data=(X_tst, Y_tst),
            callbacks=[save_best])
     
+# Za pomocą tej funkcji będziemy przewidywać ciągi
+def predict():
+    prediction_depth = int(input('Ile wartości przewidzieć >'))
+    inputs_count = len(data)-net_input_len
+    X=[]
+    # To trzeba tak zrobić z powodu problemów z serializacją glorotuniform
+    with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+        nn = keras.models.load_model(sys.argv[2])
+        for i in range(inputs_count):
+            x = data[i:i+net_input_len]
+            x = x.reshape(x.shape[0], 1, 1)
+            y = nn.fit()
+        X.append(y)
+        for _ in prediction_depth:
+            x = X[-1]
+            x = x.reshape(x.shape[0], 1, 1)
+            X.append(nn.fit(x))
+            print(X[-1])
+    
+    
 # Opcje do wyboru z menu
-option_menu = [find_best_parameters, teach_and_evaluate]
+option_menu = [find_best_parameters, teach_and_evaluate, predict]
     
 # Główna funkcja programu
 def main():
+    global net_input_len
+    net_input_len = int(input('Rozmiar wejścia sieci > '))
     prepare_data()
+    
     print('Co chcesz zrobić')
     print('1 - Wyszukać najlepsze parametry sieci')
     print('2 - Nauczyć sieć neuronową')
+    print('3 - Przewidzieć wartości błędów')
     print('0 - Zakończyć działanie programu')
     choice = int(input('>'))-1
     if choice == -1 : sys.exit()
