c19af76d3041ac707ad6048caf7a66030820f589
pgnys <pgnys@pjwstk.edu.pl>
2020-03-28 18:42:29 +0100

    Now all networks are saved

--- scripts/predict_clock_bias.py
+++ scripts/predict_clock_bias.py
@@ -85,7 +85,7 @@ def predict_bias(in_file: str, model_dir: str, bias_column: str, epoch_column: s
         predictions = prediction_core(network, x, preprocessor.window_size, prediction_depth)
         df = build_dataframe_from_predictions(predictions, preprocessor.initial_bias, first_epoch, epoch_step,
                                               preprocessor.scale)
-        out_file = os.path.join(output_dir,'{}.csv'.format(sat_name))
+        out_file = os.path.join(output_dir,'{}_{}.csv'.format(sat_name, net_name))
         df.to_csv(out_file)
     
 
