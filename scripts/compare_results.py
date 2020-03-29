22dcc374c761f2878daa6bb39e82a0d6e471d1b7
pgnys <pgnys@pjwstk.edu.pl>
2020-03-29 15:26:25 +0200

    Everything works :). Now for better configuration

--- scripts/compare_results.py
+++ scripts/compare_results.py
@@ -8,11 +8,14 @@ def prepare_files(lstm_dir: str, igu_dir: str, ref_dir: str):
     references = {}
     igu_predictions = {}
     lstm_predictions = {}
+    satellites = []
+    networks = []
     
     for r, d, f in os.walk(ref_dir):
         for file in f:
             if '.csv' in file:
                 satellite = file.split('.')[0]
+                satellites.append(satellite)
                 references[satellite]=(os.path.join(r, file))
                 
     for r, d, f in os.walk(igu_dir):
@@ -26,18 +29,23 @@ def prepare_files(lstm_dir: str, igu_dir: str, ref_dir: str):
             if '.csv' in file:
                 filename = file.split('.')[0]
                 satellite, network = filename.split('_')
+                networks.append(network)
                 try:
                     lstm_predictions[satellite][network]=(os.path.join(r, file))
                 except KeyError:
                     lstm_predictions[satellite] = {}
                     lstm_predictions[satellite][network]=(os.path.join(r, file))
                     
-    return references, igu_predictions, lstm_predictions
+    return references, igu_predictions, lstm_predictions, satellites, networks
 
 
-def compare_results(lstm_dir: str, igu_dir: str, ref_dir: str):
-    references, igu_predictions, lstm_predictions = prepare_files(lstm_dir, igu_dir, ref_dir)
+def compare_results(lstm_dir: str, igu_dir: str, ref_dir: str, out_file:str):
+    IGU_COLUMN = 'igu'
+    
+    references, igu_predictions, lstm_predictions, satellites, networks = prepare_files(lstm_dir, igu_dir, ref_dir)
+    rows = []
     for satellite in references.keys():
+        comparition = {}
         ref_biases = pd.read_csv(references[satellite], sep=';', header=0,
                                  parse_dates=[0], index_col=0,
                                  squeeze=True).values
@@ -47,6 +55,7 @@ def compare_results(lstm_dir: str, igu_dir: str, ref_dir: str):
         igu_p_mae = mean_absolute_error(ref_biases, igu_pred_biases)
         igu_p_mse = mean_squared_error(ref_biases, igu_pred_biases)
         print("%10s => MAE = %.4f MSE = %.4f RMS = %.4f" % ('IGU-P', igu_p_mae, igu_p_mse, sqrt(igu_p_mse)))
+        comparition[IGU_COLUMN] = igu_p_mae 
 
         for network in lstm_predictions[satellite].keys():
             lstm_pred_biases = pd.read_csv(lstm_predictions[satellite][network], sep=';',
@@ -55,6 +64,12 @@ def compare_results(lstm_dir: str, igu_dir: str, ref_dir: str):
             lstm_mae = mean_absolute_error(ref_biases, lstm_pred_biases)
             lstm_mse = mean_squared_error(ref_biases, lstm_pred_biases)
             print("%10s => MAE = %.4f MSE = %.4f RMS = %.4f" % ('LSTM', lstm_mae, lstm_mse, sqrt(lstm_mse)))
+            comparition[network] = lstm_mae
+        print(comparition)
+        rows.append(comparition)
+    comparitions = pd.DataFrame(rows)
+    comparitions['best'] = comparitions.loc[:, [IGU_COLUMN]+networks].idxmin(axis=1)
+    comparitions.to_csv(out_file, sep=';', index=False)
 
 def parse_arguments()-> argparse.ArgumentParser:
     desc = '''Script uses provided input data to teach a neural network'''
@@ -62,9 +77,10 @@ def parse_arguments()-> argparse.ArgumentParser:
     parser.add_argument('-l', '--lstm_dir', help='directory with lstm predictions')
     parser.add_argument('-i', '--igu_dir', help='directory with igu predictions')
     parser.add_argument('-r', '--ref_dir', help='directory with reference')
+    parser.add_argument('-o', '--out_file', help='output file')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    compare_results(args.lstm_dir, args.igu_dir, args.ref_dir)
+    compare_results(args.lstm_dir, args.igu_dir, args.ref_dir, args.out_file)
