f5e77b002b6ee35b10754b933b98ac517dd5e080
regule <pgnys@pjwstk.edu.pl>
2020-04-30 12:04:49 +0200

    Crawler works, still have to limit what is downloaded

--- support/cddis_crawler.py
+++ support/cddis_crawler.py
@@ -1,26 +1,52 @@
 from ftplib import FTP
+import argparse
 import datetime
+import os
 
-CDDIS_FTP = 'ftp://cddis.gsfc.nasa.gov'
+CDDIS_FTP = 'igs.ensg.ign.fr'
 IGU_DIR = 'pub/igs/products/'
 
 
 
+def get_valid_directories(file_list, start_idx, end_idx):
+    valid_directories = []
+    for filename in file_list:
+        try:
+            i = int(filename)
+            if start_idx >= i >= end_idx:
+                valid_directories.append(filename)
+        except Exception as e:
+            pass
+    return valid_directories
 
 
+def crawl_igu_dir(ftp, directory, out_dir):
+    print(f'Crawling directory : {directory}')
+    ftp.cwd(directory)
+    files = ftp.nlst()
+    for filename in files:
+        if 'sp3' in filename.split('.'):
+            out_file_path = os.path.join(out_dir, filename)
+            print(f'Downloading {filename}')
+            out_file = open(out_file_path, 'wb')
+            ftp.retrbinary(f'RETR {filename}', out_file.write)
+            out_file.close()
+    print('Returning to root dir')
+    ftp.cwd('..')
 
 
-def main(ftp, start_date, end_date, out_dir):
+def main(ftp_path, start_idx, end_idx, out_dir):
     try:
-        ftp = FTP('igs.ensg.ign.fr')
+        print(f'Connecting to {ftp_path}.')
+        ftp = FTP(ftp_path)
         ftp.login()
-        ftp.cwd('pub/igs/products/')
+        print(f'Switching directory to {IGU_DIR}')
+        ftp.cwd(IGU_DIR)
         files = ftp.nlst()
-        for f in files:
-            try:
-                idx = int(f)
-            except:
-                pass
+        igu_dirs = get_valid_directories(files, start_idx, end_idx)
+        print(f'Found {len(igu_dirs)} valid directories.')
+        for directory in igu_dirs:
+            crawl_igu_dir(ftp, directory, out_dir)
         ftp.quit()
     except Exception as e:
         print(f'Error => {e}')
@@ -37,13 +63,13 @@ def parse_arguments():
                         help='directory to which files will be downloaded',
                         type=str,
                         default='output')
-    parser.add_argument('-s', '--start_date',
+    parser.add_argument('-s', '--start_idx',
                         help='csv with clock bias',
-                        type= lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+                        type= int,
                         required=True)
-    parser.add_argument('-e', '--end_date',
+    parser.add_argument('-e', '--end_idx',
                         help='csv with clock bias',
-                        type= lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+                        type=int,
                         required=True)
 
     return parser.parse_args()
@@ -51,4 +77,4 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
-    main(args.ftp, args.start_date, args.end_date)
+    main(args.ftp, args.start_idx, args.end_idx, args.output)
